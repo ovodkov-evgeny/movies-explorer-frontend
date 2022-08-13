754b44146d1e79f6b38e3afb34a886fd2dfb02d5
Evgeny Ovodkov <kefir4ek@gmail.com>
2022-08-13 17:41:50 +0300

    Age fix

--- src/components/MyCard/MyCard.js
+++ src/components/MyCard/MyCard.js
@@ -8,7 +8,7 @@ export default function MyCard() {
       <img className="my-card__photo" src={photo} alt="Мое фото" />
       <h3 className="my-card__heading my-card__text">Евгений</h3>
       <h4 className="my-card__subheading my-card__text">
-        {`Фронтенд-разработчик, ${new Date().getFullYear() - 1985} лет`}
+        {`Фронтенд-разработчик, ${new Date().getFullYear() - 1986} лет`}
       </h4>
       <p className="my-card__info my-card__text">
         Живу и родился в Москве. Я люблю слушать музыку, а ещё увлекаюсь баскетболом. Начал изучать программирование полтора года назад, полгода как изучаю его под присмотром команды Яндекс.Практикума. Мне настолько понравилось, что я решил опробовать себя на новом месте.
